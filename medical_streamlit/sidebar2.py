f18097519046f5b735725bdd2206f6b1f11e8129
ajsalpv <127320356+ajsalpv@users.noreply.github.com>
2024-02-05 15:49:14 +0530

    Update sidebar2.py

--- medical_streamlit/sidebar2.py
+++ medical_streamlit/sidebar2.py
@@ -232,7 +232,7 @@ def app():
         
         text=''
         if uploaded_file is not None:
-           pdf_reader = PdfReader(uploaded_file)
+            pdf_reader = PdfReader(uploaded_file)
             text = ""
             for page in pdf_reader.pages:
                 text += page.extract_text()
