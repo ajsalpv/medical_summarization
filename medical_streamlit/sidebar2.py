88d325d68af6fa43f6d86f7eec91b8829a6fee24
Ajsal <127320356+ajsalpv@users.noreply.github.com>
2024-02-13 16:31:57 +0530

    Update sidebar2.py

--- medical_streamlit/sidebar2.py
+++ medical_streamlit/sidebar2.py
@@ -1,14 +1,15 @@
 import streamlit as st 
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader 
 import requests 
 import re
 from bs4 import BeautifulSoup
 from transformers import T5ForConditionalGeneration, AutoTokenizer, pipeline
 import torch
 from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.llms import HuggingFaceHub
+from dotenv import load_dotenv
 from transformers import AutoModel, AutoTokenizer
 # from sentence_transformers import SentenceTransformer
 # from langchain.chains.question_answering import load_qa_chain
@@ -24,37 +25,46 @@ def load_medical_summarization_model():
     model_name = "Abdulkader/autotrain-medical-reports-summarizer-2484176581"
     model = T5ForConditionalGeneration.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+
     # Return the loaded model and tokenizer
     return model_name, tokenizer
 
 model_name,tokenizer=load_medical_summarization_model()
-
-
 @st.cache_resource
 def model_ans():
     # mdl='Maite89/Roberta_finetuning_semantic_similarity_stsb_multi_mt'
     mdl='sentence-transformers/all-mpnet-base-v2'
-    w_mdl='sentence-transformers/all-MiniLM-L6-v2'
+    w_mdl='dmis-lab/biobert-v1.1'
+    # w_mdl='sentence-transformers/LaBSE'
+
+    # w_mdl='sentence-transformers/all-MiniLM-L6-v2'
+
     return mdl,w_mdl
 
 ans_model,w_ans_model=model_ans()
 
+
+
 def summary(text,max_length):
+
     summarizer = pipeline("summarization", model=model_name, tokenizer=model_name)
+
     # tokenize without truncation
     inputs_no_trunc = tokenizer(text, max_length=None, return_tensors='pt', truncation=False )
 
     # get batches of tokens corresponding to the exact model_max_length
     chunk_start = 0
-    chunk_end = tokenizer.model_max_length  # == 1024 for Bart
+    chunk_end = tokenizer.model_max_length  
     inputs_batch_lst = []
     while chunk_start <= len(inputs_no_trunc['input_ids'][0]):
         
         inputs_batch = inputs_no_trunc['input_ids'][0][chunk_start:chunk_end]  # get batch of n tokens
         inputs_batch = torch.unsqueeze(inputs_batch, 0)
         inputs_batch_lst.append(inputs_batch)
-        chunk_start += tokenizer.model_max_length  # == 1024 for Bart
-        chunk_end += tokenizer.model_max_length  # == 1024 for Bart
+        chunk_start += tokenizer.model_max_length  
+        chunk_end += tokenizer.model_max_length  
+
+
 
     summary_batch_lst = []
     for inputs in inputs_batch_lst:
@@ -65,10 +75,13 @@ def summary(text,max_length):
             length_penalty=2.0,
             do_sample=False
         )[0]['summary_text']
+
         summary_batch_lst.append(summary)
+
     # join the summaries into one string
     summary_all = '\n'.join(summary_batch_lst)
     return summary_all
+
 
 
 # Define a set of unwanted symbols
@@ -87,48 +100,95 @@ def preprocess_text(text):
     return text
 
 
-# Function to extract text from a website
+
 def get_text_from_website(url):
     try:
-        # Make a GET request to the URL
+        
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for bad requests
 
         # Parse the HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Extract text from the parsed HTML
+      
         text = ' '.join([p.get_text() for p in soup.find_all('p')])
 
         # Preprocess the extracted text
         text = preprocess_text(text)
-        print(len(text))
+
         return text
     except Exception as e:
         return f"Error: {e}"
-    
 
-def get_ans(texts,query):
+
+
+# def vectorstore(texts):
+#     # texts=texts[0]
+#     print('type of the docs is : ',type(texts),'length of the docs: ',len(texts))
+#     # embeddings = HuggingFaceEmbeddings(model_name=ans_model)
+        
+#     # print('embeddign')
+#     # db = FAISS.from_texts(texts, embeddings)
+#     print('vector')
+#     # retriever=db.as_retriever(search_type='similarity_score_threshold', search_kwargs={"score_threshold": 0.5})
+#     # docs = retriever.get_relevant_documents(query)
+#     # print('type of the docs is : ',type(docs),'length of the docs: ',len(docs))
+#     # return docs
+#     return db
+
+
+
+def get_pdf_texts_db( uploaded_file):
+    pdf_reader = PdfReader(uploaded_file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    text=text.replace("\n", "")
+    text_splitter = RecursiveCharacterTextSplitter(
+    separators=['\n\n', '\n', '.', ','],
+    chunk_size=1000,
+    chunk_overlap=200,
+    )
+    texts = text_splitter.split_text(text)
+
     embeddings = HuggingFaceEmbeddings(model_name=ans_model)
         
     print('embeddign')
-    db = FAISS.from_texts(texts, embeddings)
-    print('vector')
-    retriever=db.as_retriever(search_type='similarity_score_threshold', search_kwargs={"score_threshold": 0.5})
+    pdf_db = FAISS.from_texts(texts, embeddings)
+    return pdf_db
+
+
+def get_ans(query):
+    
+    retriever=pdf_db.as_retriever(search_type='similarity_score_threshold', search_kwargs={"score_threshold": 0.5})
     docs = retriever.get_relevant_documents(query)
+    print('type of the docs is : ',type(docs),'length of the docs: ',len(docs))
     return docs
 
 
-def get_ans_web(texts,query):
-    # texts=texts[0]
-    print('type of the docs is : ',type(texts),'length of the docs: ',len(texts))
+def get_web_text_db(website_url):
+    text=""
+    text = get_text_from_website(website_url)
+    print(len(text))
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    )
+    texts=text_splitter.split_text(text)
     embeddings = HuggingFaceEmbeddings(model_name=w_ans_model)
         
     print('embeddign')
-    db = FAISS.from_texts(texts, embeddings)
+    web_db = FAISS.from_texts(texts, embeddings)
     print('vector')
+    return web_db,embeddings
+
+
+
+def get_ans_web(query):
+    # texts=texts[0]
+    
     embedding_vector = embeddings.embed_query(query)
-    docs=db.similarity_search_by_vector(embedding_vector)
+    docs=web_db.similarity_search_by_vector(embedding_vector)
     print('type of the docs is : ',type(docs),'length of the docs: ',len(docs))
     return docs
 
@@ -142,7 +202,11 @@ def get_pdf_text(uploaded_file):
 
 
 
+
 def app():
+    global pdf_db
+    global web_db
+    global embeddings
     
     # Sidebar
     with st.sidebar:
@@ -151,6 +215,12 @@ def app():
         max_length=st.slider('Maximum Length of the Summary', 100, 2000, 100)
         st.write(max_length)
         summary_button=st.button("Summary")
+
+    if uploaded_file:
+        pdf_db=get_pdf_texts_db(uploaded_file)
+    elif website_url:
+        web_db,embeddings=get_web_text_db(website_url)
+
 
     # Main content area
     col1, col2 = st.columns(2)
@@ -161,77 +231,88 @@ def app():
         container1.markdown("#### Article")
         container2=st.container(border=True,height=300)
         container2.markdown("#### Summary")
-        
         if uploaded_file is not None and summary_button:
         # Container 1 for specific content
-            with container1:
+            with container1:  
                 pdf_text = get_pdf_text(uploaded_file)
-                preprocessed_text = preprocess_text(pdf_text)
-                output = summary(preprocessed_text, max_length=max_length)
+                preprocessed_text = preprocess_text(pdf_text) 
                 st.write(preprocessed_text)
+                output = summary(preprocessed_text, max_length=max_length)
+            # Container 2 for additional content
+                
             with container2:
                 st.write(output)
+    
 
         elif website_url and summary_button:
             with container1:
+                st.markdown("#### Article")
                 extracted_text = get_text_from_website(website_url)
+                st.write("Extracted and Preprocessed Text:")
                 if "Error:" in extracted_text:
                     st.error(extracted_text)
                 else:
                     st.write(extracted_text)
                     output=summary(extracted_text,max_length=max_length)
-                    
+
             with container2:
                 st.markdown("#### Summary")
                 st.write(output)
 
+
+            
     with col2:
         st.header("Ask About the ARTICLE")
         query = st.text_input("Question: ")
         container3=st.container(border=True,height=500)
-        # container3.write('answer')
         container3.header("Answer")
-
+        
+        
         text=''
         if uploaded_file is not None:
-            pdf_reader = PdfReader(uploaded_file)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-            text=text.replace("\n", "")
-            text_splitter = RecursiveCharacterTextSplitter(
-            separators=['\n\n', '\n', '.', ','],
-            chunk_size=1000,
-            chunk_overlap=200,
-            )
-            texts = text_splitter.split_text(text)
-            print(len(texts),type(texts))
+            # pdf_reader = PdfReader(uploaded_file)
+            # text = ""
+            # for page in pdf_reader.pages:
+            #     text += page.extract_text()
+            # text=text.replace("\n", "")
+            # text_splitter = RecursiveCharacterTextSplitter(
+            # separators=['\n\n', '\n', '.', ','],
+            # chunk_size=1000,
+            # chunk_overlap=200,
+            # )
+            # texts = text_splitter.split_text(text)
+            
             if query:
                 print(query)
-                docs=get_ans(texts,query)
+                docs=get_ans(query)
                 print('length of the docs',len(docs))
                 print(type(docs))
                 ans=str(docs[0])
                 ans=ans.replace('page_content=', '')
                 container3.write(ans)
         elif website_url:
-            text=""
-            text = get_text_from_website(website_url)
-            print(len(text))
-            text_splitter = RecursiveCharacterTextSplitter(
-            # separators=['\n\n', '\n', '.', ','],
-            chunk_size=1000,
-            chunk_overlap=200,
-            )
-            texts=text_splitter.split_text(text)
-            print(len(texts),type(texts))
+            # text=""
+            # text = get_text_from_website(website_url)
+            # print(len(text))
+            # text_splitter = RecursiveCharacterTextSplitter(
+            # chunk_size=1000,
+            # chunk_overlap=200,
+            # )
+            # texts=text_splitter.split_text(text)
+           
             if query:
                 print("link query",query)
-                wdocs=get_ans_web(texts,query)
+                wdocs=get_ans_web(query)
                 print(type(wdocs))
                 strr=str(wdocs[0])
                 strr=strr.replace('page_content=', '')
                 container3.write(strr)
+            
+
+
+                
+        
+       
 
         
         
